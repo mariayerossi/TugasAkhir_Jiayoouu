a7c7bd756323ba6e28925e3d8ad382336a1c91fb
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-07-12 15:47:54 +0700

    kontak done, all done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -389,73 +389,56 @@ Sportiva
       <div class="container" data-aos="fade-up">
 
         <div class="section-header">
-          <h2>Kontak</h2>
+          <h2>Hubungi Kami</h2>
         </div>
 
         <div class="row gx-lg-0 gy-4">
 
-          <div class="col-lg-4">
+          <div class="row gy-4">
 
-            <div class="info-container d-flex flex-column align-items-center justify-content-center">
-              <div class="info-item d-flex">
-                <i class="bi bi-geo-alt flex-shrink-0"></i>
+            <div class="col-md-6">
+              <div class="info-item  d-flex align-items-center">
+                <i class="icon bi bi-map flex-shrink-0"></i>
                 <div>
-                  <h4>Location:</h4>
-                  <p>A108 Adam Street, New York, NY 535022</p>
+                  <h3>Lokasi</h3>
+                  <p>Surabaya, Jawa Timur, Indonesia</p>
                 </div>
-              </div><!-- End Info Item -->
-
-              <div class="info-item d-flex">
-                <i class="bi bi-envelope flex-shrink-0"></i>
+              </div>
+            </div><!-- End Info Item -->
+  
+            <div class="col-md-6">
+              <div class="info-item d-flex align-items-center">
+                <i class="icon bi bi-envelope flex-shrink-0"></i>
                 <div>
-                  <h4>Email:</h4>
-                  <p>info@example.com</p>
+                  <h3>Email</h3>
+                  <p>sportiva.id@gmail.com</p>
                 </div>
-              </div><!-- End Info Item -->
-
-              <div class="info-item d-flex">
-                <i class="bi bi-phone flex-shrink-0"></i>
+              </div>
+            </div><!-- End Info Item -->
+  
+            <div class="col-md-6">
+              <div class="info-item  d-flex align-items-center">
+                <i class="icon bi bi-telephone flex-shrink-0"></i>
                 <div>
-                  <h4>Call:</h4>
-                  <p>+1 5589 55488 55</p>
+                  <h3>Telepon</h3>
+                  <p>+62 8237 5976</p>
                 </div>
-              </div><!-- End Info Item -->
-
-              <div class="info-item d-flex">
-                <i class="bi bi-clock flex-shrink-0"></i>
+              </div>
+            </div><!-- End Info Item -->
+  
+            <div class="col-md-6">
+              <div class="info-item  d-flex align-items-center">
+                <i class="icon bi bi-share flex-shrink-0"></i>
                 <div>
-                  <h4>Open Hours:</h4>
-                  <p>Mon-Sat: 11AM - 23PM</p>
+                  <h3>Jam Kerja Website</h3>
+                  <div><strong>Senin - Sabtu:</strong> 09.00 - 18.00;
+                    <strong>Minggu:</strong> Tutup
+                  </div>
                 </div>
-              </div><!-- End Info Item -->
-            </div>
-
+              </div>
+            </div><!-- End Info Item -->
+  
           </div>
-
-          <div class="col-lg-8">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="7" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-          </div><!-- End Contact Form -->
 
         </div>
 
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -18,35 +18,26 @@
         </div>
 
         <div class="col-lg-2 col-6 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
-          </ul>
+          
         </div>
 
         <div class="col-lg-2 col-6 footer-links">
-          <h4>Our Services</h4>
+          <h4>Tautan yang Berguna</h4>
           <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
+            <li><a href="#hero">Beranda</a></li>
+            <li><a href="#about">Tentang Kami</a></li>
+            <li><a href="#produk">Produk</a></li>
           </ul>
         </div>
 
         <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-          <h4>Contact Us</h4>
+          <h4>Hubungi Kami</h4>
           <p>
-            A108 Adam Street <br>
-            New York, NY 535022<br>
-            United States <br><br>
-            <strong>Phone:</strong> +1 5589 55488 55<br>
-            <strong>Email:</strong> info@example.com<br>
+            Surabaya <br>
+            Jawa Timur<br>
+            Indonesia <br><br>
+            <strong>Telepon:</strong> +62 8237 5976<br>
+            <strong>Email:</strong> sportiva.id@gmail.com<br>
           </p>
 
         </div>
