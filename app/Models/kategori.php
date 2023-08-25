5979caf1b5873b96bd04834837cab56d4e8acdac
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-08-25 19:11:25 +0700

    haaa??

---
+++ app/Models/kategori.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kategori extends Model
+{
+    use HasFactory;
+    //-------- !HARUS ADA! ------------
+    protected $table ="kategori";//diganti dengan nama table di database
+    protected $primaryKey ="id_kategori";//diganti dengan nama primary key dari table
+    public $timestamp = true; //klo true otomatis akan nambah field create_at dan update_at
+    public $incrementing = true;//utk increment
+    //---------------------------------
+
+    public function insertKategori($data)
+    {
+        $kategori = new kategori();
+        $kategori->nama_kategori = $data["nama"];
+        $kategori->save();
+    }
+
+    public function get_all_data(){
+        // return kategori::where('status',"=","1")->get();
+    }
+}
