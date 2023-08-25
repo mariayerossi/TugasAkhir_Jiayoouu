6ea6ec3618edfc234ba056fb20a6d075c156c825
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-08-25 15:15:55 +0700

    oo iyo master kategori lali

--- resources/views/pemilik/masterAlat.blade.php
+++ resources/views/pemilik/masterAlat.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.sidebarNavbar_pemilik')
 
 @section('content')
-
+<div class="container mt-5">
+    <h3 class="text-center mb-5">Tambah Alat Olahraga</h3>
+    @include("layouts.message")
+    
+    <form action="" method="post">
+        @csrf
+        <h6>Nama Alat Olahraga :</h6>
+    </form>
+</div>
 @endsection
