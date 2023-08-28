4408d5958d984c16cc55391720eaebcca7e12a26
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-08-28 18:22:20 +0700

    nambah dikit

--- resources/views/pemilik/detailAlat.blade.php
+++ resources/views/pemilik/detailAlat.blade.php
@@ -6,6 +6,22 @@
         background-color: white;
         box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
     }
+    .carousel-item {
+        position: relative;
+        width: 100%;
+        padding-bottom: 100%; /* Membuat rasio 1:1 */
+        overflow: hidden;
+    }
+
+    .carousel-item img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
 </style>
 <div class="container mt-5 p-5" >
     <div class="row">
