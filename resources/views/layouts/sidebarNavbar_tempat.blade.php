223cfcd6a8189475600f495930c8d5ba6e748042
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-08-29 17:59:54 +0700

    sidebar tempat

---
+++ resources/views/layouts/sidebarNavbar_tempat.blade.php
@@ -0,0 +1,213 @@
+@section('title')
+Sportiva
+@endsection
+
+@include('layouts.main')
+
+<body>
+    <style>
+        body {
+            font-family: "Open Sans", system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            margin: 0;
+            padding: 0;
+            transition: margin-left 0.5s;
+            background-color: #f3feff;
+        }
+    
+        #sidebar {
+            width: 250px;
+            position: fixed;
+            top: 0;
+            left: -250px;
+            height: 100%;
+            background-color: white;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            overflow-x: hidden;
+            transition: 0.5s;
+            padding-top: 60px;
+            margin-top: 20px;
+            color: rgba(255, 255, 255, 0.669);
+        }
+    
+        #sidebar a {
+            font-family: "Poppins", sans-serif;
+            padding: 10px 15px;
+            font-weight: 600;
+            text-decoration: none;
+            font-size: 16px;
+            color: #007466;
+            display: block;
+            transition: 0.3s;
+        }
+    
+        #sidebar a:hover {
+            background-color: #007466;
+            color: white;
+        }
+    
+        #sidebar .closebtn {
+            position: absolute;
+            top: 45px;
+            right: 25px;
+            font-size: 36px;
+            margin-left: 50px;
+        }
+    
+        #main {
+            transition: margin-left 0.5s;
+            margin-top: 100px;
+        }
+    
+        @media screen and (max-height: 450px) {
+            #sidebar {padding-top: 15px;}
+            #sidebar a {font-size: 18px;}
+        }
+
+        /* Style for sidebar dropdown */
+        .sidebar-dropdown-content {
+            display: none;
+            position: relative;
+            background-color: #f9f9f9;
+            min-width: 240px; /* match the sidebar width */
+            box-shadow: none; /* you may want to remove this or adjust according to the sidebar's look */
+            z-index: 1;
+        }
+
+        .sidebar-dropdown:hover .sidebar-dropdown-content {
+            display: block;
+        }
+
+        /* Adjust position of dropdown items */
+        .sidebar-dropdown-content a {
+            padding-left: 30px;
+        }
+
+        .sidebar-dropdown-content a:hover {
+            background-color: #007466;
+            color: white;
+        }
+
+
+        /* Tambahkan CSS untuk navbar sederhana */
+        nav {
+            background-color: white;
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+            padding: 10px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            position: fixed;       /* Menjadikan navbar tetap di posisi saat digulir */
+            top: 0;                /* Menempatkan navbar di bagian atas */
+            left: 0;               /* Menjadikan navbar meregang ke sisi kiri */
+            right: 0;              /* Menjadikan navbar meregang ke sisi kanan */
+            z-index: 1000;
+        }
+        nav a {
+            color: white;
+            margin: 0 10px;
+            text-decoration: none;
+        }
+        nav .coba a:hover {
+            color: #ffffff;
+            background-color: #007466
+        }
+
+        /* Style untuk foto profil dan dropdown */
+        .profile-image {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%; /* membuat gambar menjadi bulat */
+            cursor: pointer;
+        }
+
+        .profile-dropdown {
+            position: relative;
+            display: inline-block;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            right: 0;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+        }
+
+        .profile-dropdown:hover .dropdown-content {
+            display: block;
+        }
+    </style>
+    
+    <div id="sidebar">
+        <a href="/berandaPemilik"><i class="bi bi-house me-3"></i>Beranda</a>
+        <div class="sidebar-dropdown">
+            <a href="#"><i class="bi bi-dribbble me-3"></i>Alat Olahraga <i class="bi bi-caret-down-fill"></i></a>
+            <div class="sidebar-dropdown-content">
+                <a href="/masterAlatdiPemilik">Tambah Alat</a>
+                <a href="/daftarAlatdiPemilik">Daftar Alat</a>
+                <!-- Add other sports or categories here -->
+            </div>
+        </div>
+        <a href=""><i class="bi bi-clipboard-data me-3"></i>Laporan</a>
+    </div>
+
+    <div id="main">
+        <!-- Tambahkan navbar sederhana di sini -->
+        <nav>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16" onclick="toggleNav()" style="cursor: pointer">
+                <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+            {{-- logo --}}
+            <a href="" class="logo d-flex align-items-center">
+                <img class="w-20 h-20" src="{{ asset('logo2.ico')}} " alt="Logo" width="40">
+                <h2 style="font-family: 'Bruno Ace SC', cursive; color:#007466">sportiva</h2>
+            </a>
+            <div class="coba">
+                <div class="profile-dropdown">
+                    <img src="../assets/img/user_icon.png" alt="Profile" class="profile-image">
+                    <div class="dropdown-content">
+                        <a href="/editprofile">Profile</a>
+                        <a href="/logout">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Konten utama Anda -->
+        @yield('content')
+
+    </div>
+    <script>
+        openNav();
+        function openNav() {
+            document.getElementById("sidebar").style.left = "0";
+            document.getElementById("main").style.marginLeft = "250px";
+        }
+    
+        function closeNav() {
+            document.getElementById("sidebar").style.left = "-250px";
+            document.getElementById("main").style.marginLeft= "0";
+        }
+
+        function toggleNav() {
+            if (document.getElementById("sidebar").style.left == "-250px") {
+                document.getElementById("sidebar").style.left = "0";
+                document.getElementById("main").style.marginLeft = "250px";
+            }
+            else {
+                document.getElementById("sidebar").style.left = "-250px";
+                document.getElementById("main").style.marginLeft= "0";
+            }
+        }
+    </script>
+    
+</body>
