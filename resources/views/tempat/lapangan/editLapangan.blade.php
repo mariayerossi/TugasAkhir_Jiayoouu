f2aab31921837ff92e58412b82205bff84ffc001
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-09-12 20:42:12 +0700

    nguantokkk tp masi kurang banyak hiks

--- resources/views/tempat/lapangan/editLapangan.blade.php
+++ resources/views/tempat/lapangan/editLapangan.blade.php
@@ -183,7 +183,7 @@
         </div>
         <input type="hidden" name="pemilik" value="{{Session::get("dataRole")->id_tempat}}">
         <hr>
-        <h4>Atur Jam Buka Lapangan</h4>
+        <h4>Atur Jam Operasional Lapangan</h4>
         <div id="inputContainer" class="mb-5 mt-3">
             @if (!$slot->isEmpty())
                 @foreach ($slot as $item)
@@ -203,36 +203,66 @@
                         <div class="col">
                             <select id="buka1" name="buka1" class="form-select">
                                 <option value="" disabled selected>Masukkan Jam Buka</option>
-                                <option value="01:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "01:00" ? 'selected' : '' }}>01:00</option>
-                                <option value="02:00">02:00</option>
-                                <option value="03:00">03:00</option>
-                                <option value="04:00">04:00</option>
-                                <option value="05:00">05:00</option>
-                                <option value="06:00">06:00</option>
-                                <option value="07:00">07:00</option>
-                                <option value="08:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "08:00" ? 'selected' : '' }}>08:00</option>
-                                <option value="09:00">09:00</option>
-                                <option value="10:00">10:00</option>
-                                <option value="11:00">11:00</option>
-                                <option value="12:00">12:00</option>
-                                <option value="13:00">13:00</option>
-                                <option value="14:00">14:00</option>
-                                <option value="15:00">15:00</option>
-                                <option value="16:00">16:00</option>
-                                <option value="17:00">17:00</option>
-                                <option value="18:00">18:00</option>
-                                <option value="19:00">19:00</option>
-                                <option value="20:00">20:00</option>
-                                <option value="21:00">21:00</option>
-                                <option value="22:00">22:00</option>
-                                <option value="23:00">23:00</option>
-                                <option value="24:00">24:00</option>
+                                <option value="01:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "01:00:00" ? 'selected' : '' }}>01:00</option>
+                                <option value="02:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "02:00:00" ? 'selected' : '' }}>02:00</option>
+                                <option value="03:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "03:00:00" ? 'selected' : '' }}>03:00</option>
+                                <option value="04:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "04:00:00" ? 'selected' : '' }}>04:00</option>
+                                <option value="05:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "05:00:00" ? 'selected' : '' }}>05:00</option>
+                                <option value="06:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "06:00:00" ? 'selected' : '' }}>06:00</option>
+                                <option value="07:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "07:00:00" ? 'selected' : '' }}>07:00</option>
+                                <option value="08:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "08:00:00" ? 'selected' : '' }}>08:00</option>
+                                <option value="09:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "09:00:00" ? 'selected' : '' }}>09:00</option>
+                                <option value="10:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "10:00:00" ? 'selected' : '' }}>10:00</option>
+                                <option value="11:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "11:00:00" ? 'selected' : '' }}>11:00</option>
+                                <option value="12:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "12:00:00" ? 'selected' : '' }}>12:00</option>
+                                <option value="13:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "13:00:00" ? 'selected' : '' }}>13:00</option>
+                                <option value="14:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "14:00:00" ? 'selected' : '' }}>14:00</option>
+                                <option value="15:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "15:00:00" ? 'selected' : '' }}>15:00</option>
+                                <option value="16:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "16:00:00" ? 'selected' : '' }}>16:00</option>
+                                <option value="17:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "17:00:00" ? 'selected' : '' }}>17:00</option>
+                                <option value="18:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "18:00:00" ? 'selected' : '' }}>18:00</option>
+                                <option value="19:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "19:00:00" ? 'selected' : '' }}>19:00</option>
+                                <option value="20:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "20:00:00" ? 'selected' : '' }}>20:00</option>
+                                <option value="21:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "21:00:00" ? 'selected' : '' }}>21:00</option>
+                                <option value="22:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "22:00:00" ? 'selected' : '' }}>22:00</option>
+                                <option value="23:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "23:00:00" ? 'selected' : '' }}>23:00</option>
+                                <option value="24:00" {{ old('buka'.$loop->iteration) ?? $item->jam_buka == "24:00:00" ? 'selected' : '' }}>24:00</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <select id="tutup1" name="tutup1" class="form-select">
+                                <option value="" disabled selected>Masukkan Jam Tutup</option>
+                                <option value="01:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "01:00:00" ? 'selected' : '' }}>01:00</option>
+                                <option value="02:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "02:00:00" ? 'selected' : '' }}>02:00</option>
+                                <option value="03:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "03:00:00" ? 'selected' : '' }}>03:00</option>
+                                <option value="04:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "04:00:00" ? 'selected' : '' }}>04:00</option>
+                                <option value="05:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "05:00:00" ? 'selected' : '' }}>05:00</option>
+                                <option value="06:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "06:00:00" ? 'selected' : '' }}>06:00</option>
+                                <option value="07:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "07:00:00" ? 'selected' : '' }}>07:00</option>
+                                <option value="08:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "08:00:00" ? 'selected' : '' }}>08:00</option>
+                                <option value="09:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "09:00:00" ? 'selected' : '' }}>09:00</option>
+                                <option value="10:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "10:00:00" ? 'selected' : '' }}>10:00</option>
+                                <option value="11:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "11:00:00" ? 'selected' : '' }}>11:00</option>
+                                <option value="12:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "12:00:00" ? 'selected' : '' }}>12:00</option>
+                                <option value="13:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "13:00:00" ? 'selected' : '' }}>13:00</option>
+                                <option value="14:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "14:00:00" ? 'selected' : '' }}>14:00</option>
+                                <option value="15:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "15:00:00" ? 'selected' : '' }}>15:00</option>
+                                <option value="16:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "16:00:00" ? 'selected' : '' }}>16:00</option>
+                                <option value="17:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "17:00:00" ? 'selected' : '' }}>17:00</option>
+                                <option value="18:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "18:00:00" ? 'selected' : '' }}>18:00</option>
+                                <option value="19:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "19:00:00" ? 'selected' : '' }}>19:00</option>
+                                <option value="20:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "20:00:00" ? 'selected' : '' }}>20:00</option>
+                                <option value="21:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "21:00:00" ? 'selected' : '' }}>21:00</option>
+                                <option value="22:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "22:00:00" ? 'selected' : '' }}>22:00</option>
+                                <option value="23:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "23:00:00" ? 'selected' : '' }}>23:00</option>
+                                <option value="24:00" {{ old('tutup'.$loop->iteration) ?? $item->jam_tutup == "24:00:00" ? 'selected' : '' }}>24:00</option>
                             </select>
                         </div>
                     </div>
                 @endforeach
             @endif
         </div>
+        <button type="button" class="btn btn-primary mb-3" onclick="addTimeInput()">Add</button>
         <div class="d-flex justify-content-end">
             <button type="submit" class="btn btn-success">Simpan</button>
         </div>
@@ -311,5 +341,99 @@
             });
         }
     });
+    let counter = 2;
+    function addTimeInput() {
+        const container = document.getElementById('inputContainer');
+        
+        const row = document.createElement('div');
+        row.className = 'row mb-3';
+
+        const colHari = document.createElement('div');
+        colHari.className = 'col';
+        colHari.innerHTML = `
+            <select id="hari${counter}" name="hari${counter}" class="form-select">
+                <option value="" disabled selected>Masukkan Hari</option>
+                <option value="Senin">Senin</option>
+                <option value="Selasa">Selasa</option>
+                <option value="Rabu">Rabu</option>
+                <option value="Kamis">Kamis</option>
+                <option value="Jumat">Jumat</option>
+                <option value="Sabtu">Sabtu</option>
+                <option value="Minggu">Minggu</option>
+            </select>
+        `;
+
+        const colBuka = document.createElement('div');
+        colBuka.className = 'col';
+        colBuka.innerHTML = `
+        <select id="buka${counter}" name="buka${counter}" class="form-select">
+            <option value="" disabled selected>Masukkan Jam Buka</option>
+            <option value="01:00">01:00</option>
+            <option value="02:00">02:00</option>
+            <option value="03:00">03:00</option>
+            <option value="04:00">04:00</option>
+            <option value="05:00">05:00</option>
+            <option value="06:00">06:00</option>
+            <option value="07:00">07:00</option>
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+            <option value="14:00">14:00</option>
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+            <option value="22:00">22:00</option>
+            <option value="23:00">23:00</option>
+            <option value="24:00">24:00</option>
+        </select>
+        `;
+
+        const colTutup = document.createElement('div');
+        colTutup.className = 'col';
+        colTutup.innerHTML = `
+        <select id="tutup${counter}" name="tutup${counter}" class="form-select">
+            <option value="" disabled selected>Masukkan Jam Tutup</option>
+            <option value="01:00">01:00</option>
+            <option value="02:00">02:00</option>
+            <option value="03:00">03:00</option>
+            <option value="04:00">04:00</option>
+            <option value="05:00">05:00</option>
+            <option value="06:00">06:00</option>
+            <option value="07:00">07:00</option>
+            <option value="08:00">08:00</option>
+            <option value="09:00">09:00</option>
+            <option value="10:00">10:00</option>
+            <option value="11:00">11:00</option>
+            <option value="12:00">12:00</option>
+            <option value="13:00">13:00</option>
+            <option value="14:00">14:00</option>
+            <option value="15:00">15:00</option>
+            <option value="16:00">16:00</option>
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+            <option value="22:00">22:00</option>
+            <option value="23:00">23:00</option>
+            <option value="24:00">24:00</option>
+        </select>
+        `;
+
+        row.appendChild(colHari);
+        row.appendChild(colBuka);
+        row.appendChild(colTutup);
+        container.appendChild(row);
+
+        counter++;
+    }
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 @endsection
