c74576db708a300bbd9ebf98692d96ca433396d2
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-09-13 18:01:30 +0700

    enakkan curhat di git woakwoak

--- resources/views/tempat/transaksi/detailTransaksi.blade.php
+++ resources/views/tempat/transaksi/detailTransaksi.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.sidebarNavbar_tempat')
 
 @section('content')
+<div class="container mt-5 mb-5 bg-white p-4 rounded" style="box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);">
+    <h3 class="text-center mb-5">Detail Transaksi</h3>
+</div>
 @endsection
