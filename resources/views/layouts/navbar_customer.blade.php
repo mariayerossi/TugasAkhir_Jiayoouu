0150ba016e1da59f448a391dd72c82da5cbf711f
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-09-30 22:10:41 +0700

    bobokkkk

--- resources/views/layouts/navbar_customer.blade.php
+++ resources/views/layouts/navbar_customer.blade.php
@@ -139,7 +139,7 @@ Sportiva
                 </svg>
             </div>
             {{-- logo --}}
-            <a href="" class="logo d-flex align-items-center">
+            <a href="/customer/beranda" class="logo d-flex align-items-center">
                 <img class="w-20 h-20" src="{{ asset('logo2.ico')}} " alt="Logo" width="40">
                 <h2 style="font-family: 'Bruno Ace SC', cursive; color:#007466">sportiva</h2>
             </a>
@@ -165,11 +165,13 @@ Sportiva
                 </form>
             </div>
             <div class="coba">
+                {{-- cart --}}
                 <a href="">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
                         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
                     </svg>
                 </a>
+                {{-- riwyat transaksi --}}
                 <a href="">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
                         <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z"/>
@@ -236,7 +238,6 @@ Sportiva
             <a href="" class="ms-3"><i class="bi bi-cart2 me-2"></i>Keranjang</a>
             <a href="" class="ms-3"><i class="bi bi-clock-history me-2"></i>Riwayat Transaksi</a>
         </div>
-        
 
         <!-- Konten utama Anda -->
         @yield('content')
