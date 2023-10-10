8b838d23e2699e174bc77d4a0b4fe37f34f2ea01
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-10-10 23:07:23 +0700

    gakuat ngantukk

--- app/Http/Controllers/KerusakanAlat.php
+++ app/Http/Controllers/KerusakanAlat.php
@@ -8,17 +8,36 @@ use Illuminate\Http\Request;
 class KerusakanAlat extends Controller
 {
     public function ajukanKerusakan(Request $request) {
-        $request->validate([
-            'rusak' => 'required',
-            'unsur' => 'required',
-            'foto' => 'required|max:5120'
-        ],[
-            "rusak.required" => "input alat olahraga tidak boleh kosong!",
-            "unsur.required" => "input unsur kesengajaan tidak boleh kosong!",
-            "foto.max" => "ukuran foto bukti tidak boleh melebihi 5 MB!",
-            "foto.required" => "input foto tidak boleh kosong!"
-        ]);
+        // $request->validate([
+        //     'rusak' => 'required',
+        //     'unsur' => 'required',
+        //     'foto' => 'required|max:5120'
+        // ],[
+        //     "rusak.required" => "input alat olahraga tidak boleh kosong!",
+        //     "unsur.required" => "input unsur kesengajaan tidak boleh kosong!",
+        //     "foto.max" => "ukuran foto bukti tidak boleh melebihi 5 MB!",
+        //     "foto.required" => "input foto tidak boleh kosong!"
+        // ]);
 
-        dd($request->rusak);
+        $data = $request->all();
+
+        //form utama
+        if (isset($data['rusak'])) {
+            $value = $data['rusak'];
+            dd($value);
+        }
+
+        //form tambahan
+        $isi = [];
+        foreach ($data as $key => $value) {
+            if (strpos($key, 'rusak-') === 0) {
+                array_push($isi,[
+                    "alat" => $value
+                ]);
+            }
+    
+            // Anda dapat menambahkan logika serupa untuk input lain yang Anda miliki
+        }
+        dd($isi);
     }
 }
--- resources/views/tempat/transaksi/detailKerusakan.blade.php
+++ resources/views/tempat/transaksi/detailKerusakan.blade.php
@@ -44,73 +44,115 @@
             width: 180px; /* dari 140px menjadi 160px */
         }
     }
+    .card-form {
+        border: 1px solid #e0e0e0;
+        border-radius: 5px;
+        padding: 20px;
+        margin-bottom: 20px;
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+        background-color: white;
+    }
     </style>
 @include("layouts.message")
-<div class="container mt-5 mb-5 bg-white p-4 rounded" style="box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);">
+<div class="container mt-5 mb-5 p-4 rounded">
     <form action="/tempat/kerusakan/ajukanKerusakan" method="post" enctype="multipart/form-data">
         @csrf
-        <div class="d-flex justify-content-center">
-            <h3 class="text-center mb-5">Ajukan Kerusakan Alat</h3>
-        </div>
-        <div class="row mb-3">
-            <div class="col-md-4 col-12 mt-2">
-                <h6>Pilih Alat Olahraga yang Rusak</h6>
+        <div class="form-template card-form">
+            <div class="d-flex justify-content-center">
+                <h3 class="text-center mb-5">Ajukan Kerusakan Alat</h3>
             </div>
-            <div class="col-md-8 col-12 mt-2 mt-md-0 mb-3 d-flex flex-wrap">
-                @foreach ($dtrans as $item)
-                    @php
-                        $dataAlat = DB::table('alat_olahraga')->where("id_alat","=",$item->fk_id_alat)->get()->first();
-                        $dataFileAlat = DB::table('files_alat')->where("fk_id_alat","=",$item->fk_id_alat)->get()->first();
-                    @endphp
-                    <div class="custom-radio-container">
-                        <input type="radio" class="btn-check" name="rusak" id="alat-{{$dataAlat->id_alat}}-{{$item->id_dtrans}}" autocomplete="off" value="{{$dataAlat->id_alat}}-{{$item->id_dtrans}}">
-                        <label class="btn btn-outline-primary" for="alat-{{$dataAlat->id_alat}}-{{$item->id_dtrans}}">
-                            <div class="card tiny-card h-70 mb-1 mr-1">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <!-- Gambar Alat -->
-                                        <div class="col-4">
-                                            <div class="square-image-container2">
-                                                <img src="{{ asset('upload/' . $dataFileAlat->nama_file_alat) }}" alt="" class="img-fluid">
+            <div class="row mb-3">
+                <div class="col-md-4 col-12 mt-2">
+                    <h6>Pilih Alat Olahraga yang Rusak</h6>
+                </div>
+                <div class="col-md-8 col-12 mt-2 mt-md-0 mb-3 d-flex flex-wrap">
+                    @foreach ($dtrans as $item)
+                        @php
+                            $dataAlat = DB::table('alat_olahraga')->where("id_alat","=",$item->fk_id_alat)->get()->first();
+                            $dataFileAlat = DB::table('files_alat')->where("fk_id_alat","=",$item->fk_id_alat)->get()->first();
+                        @endphp
+                        <div class="custom-radio-container">
+                            <input type="radio" class="btn-check" name="rusak" id="alat-{{$dataAlat->id_alat}}-{{$item->id_dtrans}}" autocomplete="off" value="{{$dataAlat->id_alat}}-{{$item->id_dtrans}}">
+                            <label class="btn btn-outline-primary" for="alat-{{$dataAlat->id_alat}}-{{$item->id_dtrans}}">
+                                <div class="card tiny-card h-70 mb-1 mr-1">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <!-- Gambar Alat -->
+                                            <div class="col-4">
+                                                <div class="square-image-container2">
+                                                    <img src="{{ asset('upload/' . $dataFileAlat->nama_file_alat) }}" alt="" class="img-fluid">
+                                                </div>
                                             </div>
-                                        </div>
-                                        
-                                        <!-- Nama Alat -->
-                                        <div class="col-8 d-flex align-items-center justify-content-between">
-                                            <h5 class="card-title truncate-text">{{$dataAlat->nama_alat}}</h5>
+                                            
+                                            <!-- Nama Alat -->
+                                            <div class="col-8 d-flex align-items-center justify-content-between">
+                                                <h5 class="card-title truncate-text">{{$dataAlat->nama_alat}}</h5>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </label>
-                    </div>
-                @endforeach
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col-md-4 col-12 mt-2">
-                <h6>Apakah terdapat unsur kesengajaan dalam kerusakan alat olahraga?</h6>
-            </div>
-            <div class="col-md-8 col-12 mt-2 mt-md-0 mb-3">
-                <input type="radio" class="btn-check" name="unsur" id="danger-outlined" autocomplete="off" value="Ya">
-                <label class="btn btn-outline-danger" for="danger-outlined">Ya</label>
+            <div class="row mb-3">
+                <div class="col-md-4 col-12 mt-2">
+                    <h6>Apakah terdapat unsur kesengajaan dalam kerusakan alat olahraga?</h6>
+                </div>
+                <div class="col-md-8 col-12 mt-2 mt-md-0 mb-3">
+                    <input type="radio" class="btn-check" name="unsur" id="danger-outlined" autocomplete="off" value="Ya">
+                    <label class="btn btn-outline-danger" for="danger-outlined">Ya</label>
 
-                <input type="radio" class="btn-check" name="unsur" id="primary-outlined" autocomplete="off" value="Tidak">
-                <label class="btn btn-outline-primary" for="primary-outlined">Tidak</label>
+                    <input type="radio" class="btn-check" name="unsur" id="primary-outlined" autocomplete="off" value="Tidak">
+                    <label class="btn btn-outline-primary" for="primary-outlined">Tidak</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-12 mt-2">
+                    <h6>Lampirkan Bukti</h6>
+                </div>
+                <div class="col-md-8 col-12 mt-2 mt-md-0 mb-3">
+                    <input type="file" class="form-control" name="foto" accept=".jpg,.png,.jpeg">
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-4 col-12 mt-2">
-                <h6>Lampirkan Bukti</h6>
-                <span style="font-size: 14px">lampirkan 2 file sekaligus</span>
-            </div>
-            <div class="col-md-8 col-12 mt-2 mt-md-0 mb-3">
-                <input type="file" class="form-control" name="foto[]" multiple accept=".jpg,.png,.jpeg">
-            </div>
-        </div>
-        <div class="d-flex justify-content-end">
+        <div class="d-flex justify-content-between mt-4">
+            <button type="button" class="btn btn-primary" id="addFormBtn">Tambah</button>
             <button type="submit" class="btn btn-success">Kirim</button>
         </div>
     </form>
 </div>
+<script>
+    document.getElementById("addFormBtn").addEventListener("click", function() {
+    let template = document.querySelector(".form-template").outerHTML;
+    let container = document.createElement("div");
+    container.innerHTML = template;
+
+    // Mencari semua elemen input di dalam container
+    let inputs = container.querySelectorAll("input");
+    let uniqueTime = new Date().getTime();  // Dapatkan timestamp sebagai ID unik
+
+    // Loop melalui setiap input
+    inputs.forEach((input, index) => {
+        if (input.id) {
+            let newId = input.id + "-" + uniqueTime + "-" + index;
+            let associatedLabel = container.querySelector("label[for='" + input.id + "']");
+            
+            // Modifikasi nama radio button agar unik untuk setiap form
+            if (input.type === "radio") {
+                input.name = input.name + "-" + uniqueTime;
+            }
+
+            input.id = newId;
+            if (associatedLabel) {
+                associatedLabel.setAttribute("for", newId);
+            }
+        }
+    });
+
+    document.querySelector("form").insertBefore(container, document.getElementById("addFormBtn").parentNode);
+});
+
+</script>
 @endsection
