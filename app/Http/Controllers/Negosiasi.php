3212b78037eb8737e12fd2ffae80ee8e13b64b46
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-10-11 22:35:06 +0700

    ngantuk polll

--- app/Http/Controllers/Negosiasi.php
+++ app/Http/Controllers/Negosiasi.php
@@ -48,12 +48,40 @@ class Negosiasi extends Controller
         $nego->insertNegosiasi($data);
 
         if (Session::get("role") == "pemilik") {
+            //yang kirim pemilik
             $dataPemilik = DB::table('pemilik_alat')->where("id_pemilik","=",Session::get("dataRole")->id_pemilik)->get()->first();
             $user = $dataPemilik->nama_pemilik;
+
+            //kasih notif ke tempat
+            $permintaan = DB::table('request_permintaan')->where("id_permintaan","=",$request->permintaan)->get()->first();
+            $tempat = DB::table('pihak_tempat')->where("id_tempat","=",$permintaan->fk_id_tempat)->get()->first();
+
+            $dataNotif = [
+                "subject" => "Negosiasi Permintaan Baru",
+                "judul" => "Negosiasi Permintaan Baru Dari ".$user,
+                "nama_user" => $tempat->nama_tempat,
+                "isi" => ""
+            ];
+            $e = new notifikasiEmail();
+            $e->sendEmail($tempat->email_tempat,$dataNotif);
         }
         else if (Session::get("role") == "tempat") {
+            //yang kirim tempat
             $dataTempat = DB::table('pihak_tempat')->where("id_tempat","=",Session::get("dataRole")->id_tempat)->get()->first();
             $user = $dataTempat->nama_pemilik_tempat;
+
+            //kasih notif ke pemilik
+            $permintaan = DB::table('request_permintaan')->where("id_permintaan","=",$request->permintaan)->get()->first();
+            $pemilik = DB::table('pemilik_alat')->where("id_pemilik","=",$permintaan->fk_id_pemilik)->get()->first();
+
+            $dataNotif = [
+                "subject" => "Negosiasi Permintaan Baru",
+                "judul" => "Negosiasi Permintaan Baru Dari ".$user,
+                "nama_user" => $pemilik->nama_pemilik,
+                "isi" => ""
+            ];
+            $e = new notifikasiEmail();
+            $e->sendEmail($pemilik->email_pemilik,$dataNotif);
         }
 
         $tanggalAwal = $waktu;
@@ -102,12 +130,18 @@ class Negosiasi extends Controller
         $nego->insertNegosiasi($data);
 
         if (Session::get("role") == "pemilik") {
+            //yang kirim pemilik
             $dataPemilik = DB::table('pemilik_alat')->where("id_pemilik","=",Session::get("dataRole")->id_pemilik)->get()->first();
             $user = $dataPemilik->nama_pemilik;
+
+            //kirim notif ke tempat
         }
         else if (Session::get("role") == "tempat") {
+            //yang kirim tempat
             $dataTempat = DB::table('pihak_tempat')->where("id_tempat","=",Session::get("dataRole")->id_tempat)->get()->first();
             $user = $dataTempat->nama_pemilik_tempat;
+
+            //kirim notif ke pemilik
         }
 
         $tanggalAwal = $waktu;
