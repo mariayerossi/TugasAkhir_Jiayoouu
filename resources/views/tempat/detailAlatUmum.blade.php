2a89984bba1a1d35842f55ded6535c08d000b537
mariayerossi <maria_y20@mhs.istts.ac.id>
2023-11-13 11:16:41 +0700

    bisa yokkk

--- resources/views/tempat/detailAlatUmum.blade.php
+++ resources/views/tempat/detailAlatUmum.blade.php
@@ -66,7 +66,7 @@
 
 
 @if (!$alat->isEmpty())
-<div class="container mt-3 mb-3">
+<div class="container mt-5 mb-5">
     <div class="row">
         <!-- Left Section: Image, Product Name, and Rating -->
         <div class="col-lg-4 left-section">
@@ -122,6 +122,7 @@
                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                 </svg> {{ $averageRating }} rating ({{ $totalReviews }})
             </p>
+            <h5>Komisi Pemilik Alat:</h5>
             <h3>Rp {{ number_format($alat->first()->komisi_alat, 0, ',', '.') }} /jam</h3>
         </div>
 
@@ -255,6 +256,11 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();   
+    });
+</script>
 @else
     <h1>Alat Olahraga tidak tersedia</h1>
 @endif
