6fbc24bc63c1e54be0858ad6024c35fea5897362
mariayerossi <maria_y20@mhs.istts.ac.id>
2024-01-21 14:21:50 +0700

    njir ngerjar waktu lagi

--- resources/views/layouts/sidebarNavbar_tempat.blade.php
+++ resources/views/layouts/sidebarNavbar_tempat.blade.php
@@ -201,6 +201,25 @@ Sportiva
             color: #007466;
             cursor: pointer;
         }
+        .notification-container {
+            position: relative;
+            display: inline-block;
+        }
+
+        .notification-icon {
+            vertical-align: middle;
+        }
+
+        .notification-count {
+            position: absolute;
+            top: -5px;
+            right: -5px;
+            background-color: #ff4d4d;
+            color: #fff;
+            border-radius: 50%;
+            padding: 2px 6px;
+            font-size: 10px;
+        }
     </style>
 
     <div id="sidebar">
@@ -275,10 +294,14 @@ Sportiva
             @endphp
             <div class="coba">
                 <div class="notif-dropdown me-3">
-                    <div title="Notifikasi">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+                    <div class="notification-container" title="Notifikasi">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bell notification-icon" viewBox="0 0 16 16">
                             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
                         </svg>
+                        @php
+                            $jumlah = DB::table('notifikasi')->where("fk_id_tempat","=",Session::get("dataRole")->id_tempat)->where("status_notifikasi","=","Tidak")->count();    
+                        @endphp
+                        <span class="notification-count">{{$jumlah}}</span>
                     </div>
                     <div class="notif-dropdown-content ">
                         <h6 class="text-center m-3">Notifikasi</h6>
